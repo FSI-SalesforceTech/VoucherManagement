6cad95cbab05d257afe51a94aedc9d5b1084f0b1
小倉 裕之 <ogurah@fsi.co.jp>
2019-10-29 18:23:23 +0900

    master以外のリモートリポジトリにpushされると、パッケージを作成してSandboxにインストールするよう変更

--- scripts/packageManagement.sh
+++ scripts/packageManagement.sh
@@ -3,6 +3,12 @@
 SFDX_CLI_EXEC="sfdx"
 PACKAGENAME="voucherManagement"
 DEV_HUB="bunai-crm"
+SAND_HUB="sandbox"
 
-PACKAGE_VERSION="$($SFDX_CLI_EXEC force:package:version:list -p $PACKAGENAME -v $DEV_HUB)"
-echo $PACKAGE_VERSION | jq
+PACKAGE_RESULT="$($SFDX_CLI_EXEC force:package:version:create -p $PACKAGENAME -x -w 10 -v $DEV_HUB --json)"
+echo $PACKAGE_RESULT | jq
+PACKAGE_VERSION="$(echo $PACKAGE_VERSION | jq '.result.SubscriberPackageVersionId' | tr -d '"')"
+echo PACKAGE_VERSION | jq
+
+INSTALL_RESULT="$($SFDX_CLI_EXEC force:package:install --package $PACKAGE_VERSION -w 10 -u $SAND_HUB -r)"
+echo INSTALL_RESULT | jp
