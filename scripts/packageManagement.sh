9f466e6cfd07aff7c6c1982347a97251c569a304
小倉 裕之 <ogurah@fsi.co.jp>
2019-10-30 09:53:00 +0900

    installコマンドの修正

--- scripts/packageManagement.sh
+++ scripts/packageManagement.sh
@@ -7,7 +7,7 @@ SAND_HUB="sandbox"
 
 PACKAGE_RESULT="$($SFDX_CLI_EXEC force:package:version:create -p $PACKAGENAME -x -w 10 -v $DEV_HUB --json)"
 echo $PACKAGE_RESULT | jq
-PACKAGE_VERSION="$(echo $PACKAGE_VERSION | jq '.result.SubscriberPackageVersionId' | tr -d '"')"
+PACKAGE_VERSION="$(echo $PACKAGE_RESULT | jq '.result.SubscriberPackageVersionId' | tr -d '"')"
 echo PACKAGE_VERSION | jq
 
 INSTALL_RESULT="$($SFDX_CLI_EXEC force:package:install --package $PACKAGE_VERSION -w 10 -u $SAND_HUB -r)"
