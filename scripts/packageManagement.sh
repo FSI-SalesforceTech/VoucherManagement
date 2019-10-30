3f2016f6bd432893935cab8bef58ab6fd71fc7b1
小倉 裕之 <ogurah@fsi.co.jp>
2019-10-30 10:21:58 +0900

    installコマンドの修正

--- scripts/packageManagement.sh
+++ scripts/packageManagement.sh
@@ -5,11 +5,13 @@ PACKAGENAME="voucherManagement"
 DEV_HUB="bunai-crm"
 SAND_HUB="sandbox"
 
-PACKAGE_RESULT="$($SFDX_CLI_EXEC force:package:version:create -p $PACKAGENAME -x -w 10 -v $DEV_HUB --json)"
+echo "$SFDX_CLI_EXEC force:org:list"
+
+PACKAGE_RESULT="$($SFDX_CLI_EXEC force:package:version:create -p $PACKAGENAME -d force-app -x -w 10 -v $DEV_HUB --json)"
 echo $PACKAGE_RESULT | jq
 PACKAGE_VERSION="$(echo $PACKAGE_RESULT | jq '.result.SubscriberPackageVersionId' | tr -d '"')"
 echo PACKAGE_VERSION | jq
 
 INSTALL_RESULT="$($SFDX_CLI_EXEC force:package:install --package $PACKAGE_VERSION -w 10 -u $SAND_HUB -r)"
-echo "$SFDX_CLI_EXEC force:package:install --package $PACKAGE_VERSION -w 10 -u $SAND_HUB -r"
+echo "$SFDX_CLI_EXEC force:package:install --package $PACKAGE_VERSION -w 10 --publishwait 10 -u $SAND_HUB -r"
 echo INSTALL_RESULT | jp
